cf06dd9f8ff817a597e0fa853a05f466c66aa7db
Anton <mercury131@ya.ru>
2019-11-25 08:09:51 +0300

    add script

--- lesson6/log-analizer.sh
+++ lesson6/log-analizer.sh
@@ -1,3 +1,18 @@
-!#/bin/bash
+#!/bin/bash
 
 
+help () {
+  echo "Show help guide"
+}
+
+
+analize_errors () {
+
+
+  cat $1 | awk '{ print $1, $6, $7, $9}'
+  cat $1 | awk '{ print $1, $6, $7, $9}' | sort -k1 -u
+}
+
+help
+
+analize_errors $1
