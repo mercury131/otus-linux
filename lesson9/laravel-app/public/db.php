fb34b498d5b28821b8d8e14fd66ad5972cb3e14f
Anton Cherevaty <Cherevaty@avp.ru>
2019-12-04 13:26:33 +0300

    add lesson 9 (docker)

---
+++ lesson9/laravel-app/public/db.php
@@ -0,0 +1,14 @@
+ <?php
+$servername = "db";
+$username = "root";
+$password = "laravel";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?> 
